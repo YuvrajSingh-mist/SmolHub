267516694b90734c3ba7e8a4706873d003263cb8
YuvrajSingh-mist <yuvraj.singh9886@outlook.com>
2025-04-05 12:43:24 +0000

    Added project name for wandb input

--- SFT/helper/visualize.py
+++ SFT/helper/visualize.py
@@ -1,10 +1,11 @@
 import wandb
+from load_config import Config
 
-
+config = Config().get_config()
 
 class Visualizer:
     def __init__(self):
-        wandb.init(project="SFTrainer")
+        wandb.init(project=config["wandb"]["project_name"])
 
     def log(self, metrics: Dict[str, float]):
         wandb.log(metrics)
